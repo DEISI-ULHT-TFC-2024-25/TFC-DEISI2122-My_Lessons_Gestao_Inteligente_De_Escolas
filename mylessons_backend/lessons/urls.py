297f29538e95214106748c2bfd1d568f78b8a06b
Francisco-Sousa-a22301231 <franciscobarrososousa@gmail.com>
2025-02-16 22:54:51 +0000

    private lesson details endpoint

--- mylessons_backend/lessons/urls.py
+++ mylessons_backend/lessons/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import upcoming_lessons, last_lessons, schedule_private_lesson, active_packs, pack_datails
+from .views import upcoming_lessons, last_lessons, schedule_private_lesson, active_packs, private_pack_details, group_pack_details, private_lesson_details, group_lesson_details
 
 urlpatterns = [
     path('upcoming-lessons/', upcoming_lessons, name='upcoming-lessons'),
     path('last-lessons/', last_lessons, name='last-lessons'),
     path('schedule-private-lesson/', schedule_private_lesson, name='schedule-private-lesson'),
     path('active-packs/', active_packs, name='active-packs'),
-#    path('pack-datails/{id}/', pack_datails, name='pack-datails'),
-#    path('lesson-datails/{id}/', lesson_datails, name='lesson-datails'),
+    path('private-pack-details/<int:id>/', private_pack_details, name='private-pack-details'),
+    path('private-lesson-details/<int:id>/', private_lesson_details, name='private-lesson-details'),
+    path('group-pack-details/<int:id>/', group_pack_details, name='group-pack-details'),
+    path('group-lesson-details/<int:id>/', group_lesson_details, name='group-lesson-details'),
 ]
