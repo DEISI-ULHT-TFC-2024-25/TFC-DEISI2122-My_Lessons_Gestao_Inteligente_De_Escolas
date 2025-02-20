a5b33fe10e86124e27ff61eeb5a632e21d9c00c0
Francisco Sousa <franciscobarrososousa@gmail.com>
2025-02-20 23:17:14 +0000

    lessons api changes

--- mylessons_backend/lessons/admin.py
+++ mylessons_backend/lessons/admin.py
@@ -4,7 +4,7 @@ from .models import  Lesson, Pack, Voucher
 
 @admin.register(Pack)
 class PackAdmin(admin.ModelAdmin):
-    list_display = ('date', 'number_of_classes', 'number_of_classes_left', 'is_done', 'is_paid', 'debt')
+    list_display = ('date', "type", 'number_of_classes', 'number_of_classes_left', 'is_done', 'is_paid', 'debt')
     list_filter = ('is_done', 'is_paid', 'is_suspended', 'date')
     search_fields = ('students__first_name', 'students__last_name', 'instructors__user__first_name', 'instructors__user__last_name')
     ordering = ('-date',)
@@ -13,7 +13,7 @@ class PackAdmin(admin.ModelAdmin):
 
 @admin.register(Lesson)
 class LessonAdmin(admin.ModelAdmin):
-    list_display = ('date', 'start_time', 'end_time', 'level', 'maximum_number_of_students', 'is_done')
+    list_display = ('date', "type", 'start_time', 'end_time', 'level', 'maximum_number_of_students', 'is_done')
     list_filter = ('level', 'is_done', 'date')
     search_fields = ('students__first_name', 'students__last_name', 'instructors__user__first_name', 'instructors__user__last_name')
     ordering = ('-date', '-start_time')
