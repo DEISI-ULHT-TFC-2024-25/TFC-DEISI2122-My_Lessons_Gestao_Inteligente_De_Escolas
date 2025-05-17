4b1f1d8f518532c652d1ff32a4c1049835225f06
Francisco Sousa <franciscobarrososousa@gmail.com>
2025-05-17 12:23:32 +0100

    updated wsgi.py

--- mylessons_backend/mylessons/wsgi.py
+++ mylessons_backend/mylessons/wsgi.py
@@ -19,7 +19,7 @@ if not firebase_admin._apps:
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
     # Now build the full path to the JSON file inside "mylessons_backend".
-    json_path = os.path.join(BASE_DIR, "mylessons-7b4ed-firebase-adminsdk-fbsvc-d901fb586d.json")
+    json_path = os.path.join(BASE_DIR, "mylessons-7b4ed-firebase-adminsdk-fbsvc-9390c43ae3.json")
     cred = credentials.Certificate(json_path)
     firebase_admin.initialize_app(cred)
 # ───────────────────────────────────────────────────────────
